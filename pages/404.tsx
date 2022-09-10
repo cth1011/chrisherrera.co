4130c808ac29601dae8aa66260e4b66d20b6b42e
Christopher Herrera <cth0910@gmail.com>
2022-09-10 11:40:27 +0800

    Add custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,43 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import tw from "twin.macro";
 
-const Custom404 = () => <h1> 404 - Page Not Found</h1>;
+import FourZeroFour from "styles/assets/404.jpg";
+
+const Custom404 = () => (
+  <Container>
+    <h1>Whoops!</h1>
+    <small>Well this is embarassing...</small>
+    <ImageContainer>
+      <Image src={FourZeroFour} />
+    </ImageContainer>
+
+    <h2>Looks like the page went on vacation</h2>
+    <small>
+      You can head back to our{" "}
+      <Link href="/" passHref>
+        <strong>
+          <a className="nav">homepage</a>
+        </strong>
+      </Link>{" "}
+      or{" "}
+      <Link href="/blog" passHref>
+        <strong>
+          <a className="nav">blog</a>
+        </strong>
+      </Link>
+    </small>
+  </Container>
+);
 
 export default Custom404;
+
+const Container = tw.div`
+    flex flex-col items-center 
+    ml-auto mr-auto 
+    max-w-[70%] max-h-[400px]
+`;
+const ImageContainer = tw.div`
+    px-10 py-4
+`;
