f5be000b4935d3e597e855394c30fd7e2895ddbf
Christopher Herrera <cth0910@gmail.com>
2022-09-10 11:39:55 +0800

    Add heading base style

--- components/GlobalStyles.tsx
+++ components/GlobalStyles.tsx
@@ -37,6 +37,18 @@ a.post-title:hover {
   background-size: 4px 50px;
   border-bottom-color: #ffb703;
 }
+h1 {
+  font-size: 2em;
+  font-weight: bold;
+}
+h2 {
+  font-size: 1.5em;
+  font-weight: bold;
+}
+h3 {
+  font-size: 1.17em;
+  font-weight: bold;
+}
 
 `;
 
