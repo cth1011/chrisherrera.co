dbeb3d381b31e30d456e9a91b7245f8e0c7ed362
Christopher Herrera <cth0910@gmail.com>
2022-09-11 14:06:34 +0800

    Add Button

---
+++ components/Button.tsx
@@ -0,0 +1,24 @@
+import tw from "twin.macro";
+import Link from "next/link";
+
+type ButtonProps = {
+  href: "string";
+  children: React.ReactNode;
+};
+
+const Button = ({ href, children }: ButtonProps) => (
+  <Link href={href}>
+    <StyledButton type="button">{children}</StyledButton>
+  </Link>
+);
+
+export default Button;
+
+const StyledButton = tw.button`
+text-white text-sm font-medium  text-center
+bg-gradient-to-br from-purple-600 to-blue-500 
+hover:bg-gradient-to-bl focus:ring-4 focus:outline-none 
+focus:ring-blue-300 dark:focus:ring-blue-800 
+rounded-lg px-5 py-2.5 mr-2 mb-2
+inline-flex items-center transition-all duration-1000
+`;
--- components/Card.tsx
+++ components/Card.tsx
@@ -4,6 +4,8 @@ import tw from "twin.macro";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 
+import Button from "components/Button";
+
 type CardProps = {
   src?: StaticImageData;
   alt?: string;
@@ -26,7 +28,7 @@ const Card = ({
   <Container>
     {src && (
       <Link href={href}>
-        <Img src={src} alt={alt} />
+        <Img src={src} alt={alt} placeholder="blur" />
       </Link>
     )}
     <Body>
@@ -80,13 +82,4 @@ const Title = tw.h5`
 const Description = tw.p`
     my-3 font-normal 
     text-gray-700 dark:text-gray-400
-`;
-
-const Button = tw.a`
-    inline-flex items-center 
-    py-2 px-3 
-    text-sm font-medium text-center text-white 
-    bg-blue-700 rounded-lg 
-    hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
-    dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
 `;
