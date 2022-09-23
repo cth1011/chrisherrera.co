46d99782a236dd54e18ae54ae737386813f6de89
Christopher Herrera <cth0910@gmail.com>
2022-09-23 16:31:39 -0700

    Refactor projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,32 +3,71 @@ import tw from "twin.macro";
 
 import Card from "components/Card";
 
-import HIV from "styles/assets/hiv/HIV.png";
+import Japan from "public/assets/mapbox-exam/covid-japan.jpg";
+import HIV from "styles/assets/projects/HIV.png";
+import Hermosa from "styles/assets/projects/hermosa.png";
+
+const projects = [
+  {
+    title: "Personal Blog",
+    description: `Created a personal blog using NextJS, 
+    TailwindCSS, and TypeScript. It started out with GatsbyJS, but I
+    I decided to migrate the entire platform to NextJS.`,
+    src: HIV,
+    from: "Feb 2020",
+    to: "Present",
+    href: "projects/hiv",
+    tags: ["ReactJS", "TypeScript", "TailwindCSS"],
+  },
+  {
+    title: "Covid-19 Japan Tracker",
+    description: `
+    I helped a few friends setup their 
+    shopify store in the Philippines. They
+    sell over 200 products with over 20 brands.
+    `,
+    src: Japan,
+    href: "/posts/mapbox-exam",
+    from: "Jan 2020",
+    to: "Mar 2020",
+    tags: ["Shopify", "Liquid"],
+  },
+  {
+    title: "Hermosa Beauty Store",
+    description: `
+    I helped a few friends setup their 
+    shopify store in the Philippines. They
+    sell over 200 products with over 20 brands.
+    `,
+    src: Hermosa,
+    href: "https://hermosabeautystore.com/",
+    from: "Jan 2020",
+    to: "Mar 2020",
+    label: "Visit Website",
+    tags: ["Shopify", "Liquid"],
+  },
+  {
+    title: "Inaccessible HIV Centers in the Philippines",
+    description: `Developed a geospatial web application that can identify
+    inaccesible HIV centers based on the your area in the Philippines.`,
+    src: HIV,
+    from: "Sept 2018",
+    to: "Dec 2018",
+    tags: ["Mapbox JS", "Javascript"],
+    href: "https://stories.thinkingmachin.es/doh-hiv-centers-access/",
+  },
+];
 
 const Projects: React.FC = () => {
   return (
     <div>
-      <h3> Projects</h3>
+      <Title> Projects</Title>
       <Container>
-        <Item>
-          <Card
-            title="HIV Blog Post"
-            description="Worked on HIV blogpost"
-            src={HIV}
-            from="Sept 2018"
-            to="Dec 2018"
-            href="projects/hiv"
-          />
-        </Item>
-        <Item>
-          <Card
-            title="HIV Blog Post"
-            description="Worked on HIV blogpost"
-            src={HIV}
-            from="Sept 2018"
-            to="Dec 2018"
-          />
-        </Item>
+        {projects.map((project, i) => (
+          <Item key={i}>
+            <Card {...project} />
+          </Item>
+        ))}
       </Container>
     </div>
   );
@@ -36,11 +75,15 @@ const Projects: React.FC = () => {
 
 export default Projects;
 
+const Title = tw.h2`
+text-center md:text-left
+`;
+
 const Container = tw.div`
     flex flex-row flex-wrap items-center justify-center
     py-4
 `;
 
 const Item = tw.div`
-    sm:w-1/2
+    md:w-1/2 mb-2
 `;
