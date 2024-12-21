9608ac29b8bb4ce8b16c7fd33df8f93d4eb11c63
Chris Herrera <cth0910@gmail.com>
2024-12-21 14:23:08 -0800

    fix: remove outline after clicking dark mode btn

--- components/Header.tsx
+++ components/Header.tsx
@@ -77,7 +77,6 @@ const NavLink = tw.a`
 const DarkModeButton = tw.button`
  text-gray-500 dark:text-gray-400
 hover:bg-gray-100 dark:hover:bg-gray-700 
-focus:outline-none focus:ring-4 
-focus:ring-gray-200 dark:focus:ring-gray-700 
+focus:outline-none
 rounded-lg text-sm p-2.5
  `;
